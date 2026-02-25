6f07d08407248217bb4384ed5b88cd8f27edb391
Shruti Karmarkar <87332058+Shrutikarmarkar@users.noreply.github.com>
2026-02-24 21:22:02 -0500

    Update download_dataset.py

--- etl/download_dataset.py
+++ etl/download_dataset.py
@@ -1,1 +1,13 @@
+import os
+import requests
 
+DATA_DIR = "data/raw"
+FILE_PATH = os.path.join(DATA_DIR, "OnlineRetail.xlsx")
+
+def download_dataset():
+    print("Please manually download the Online Retail II dataset from:")
+    print("https://archive.ics.uci.edu/ml/datasets/online+retail+ii")
+    print(f"Place it inside: {FILE_PATH}")
+
+if __name__ == "__main__":
+    download_dataset()
